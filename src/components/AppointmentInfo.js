b3692109571cdeb56baf3f8183ef5e1db1809901
Daniel Villarreal <dvillarreal93@gmail.com>
2021-12-22 12:00:11 -0700

    Removed typo

--- src/components/AppointmentInfo.js
+++ src/components/AppointmentInfo.js
@@ -17,7 +17,7 @@ const AppointmentInfo = ({ appointment, onDeleteAppointment }) => {
           <span className="flex-grow text-right">{appointment.aptDate}</span>
         </div>
         <div>
-          <b className="font-bold text-blue-500">Owner:</b>{" "}
+          <b className="font-bold text-blue-500">Owner:</b>
           {appointment.ownerName}
         </div>
         <div className="leading-tight">{appointment.aptNotes}</div>
